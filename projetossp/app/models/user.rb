1dec6276cd541089f2dd2a30d86f2725613f9549
Rodrigo <rodrigo19962010@live.com>
2017-01-24 14:19:00 -0300

    Validação de acesso ao sistema pelo administrador feita com sucesso!

--- projetossp/app/models/user.rb
+++ projetossp/app/models/user.rb
@@ -11,6 +11,28 @@ class User < ActiveRecord::Base
   # Relacionamentos
   belongs_to :user_group
   has_many :user_permissions
+
+  def active_for_authentication? 
+    super && approved? 
+  end 
+
+  def inactive_message 
+    if !approved? 
+      :not_approved 
+    else 
+      super # Use whatever other message 
+    end 
+  end
+
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
   
   # Função de verificação de permissão dentro do módulo
   # da aplicação
--- projetossp/config/routes.rb
+++ projetossp/config/routes.rb
@@ -7,9 +7,17 @@ Rails.application.routes.draw do
   resources :permissions
   get 'auditor/index'
   get 'auditor/show/:id', to: 'auditor#show', as: :show_auditor
+
+  get 'users/change_approved/:id', to: 'users#change_approved', as: :change_approved
   
   resources :people
+  
   devise_for :users
+
+  scope "/administrador" do
+    resources :users
+  end
+
   root to: 'home#index'
   get '/about', to: 'home#about', as: :about
   get '/contact', to: 'home#contact', as: :contact
